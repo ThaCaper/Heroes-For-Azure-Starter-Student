524eab29d97184abeb06ebc4d44111956d6fa5c6
andreas <andreas-cooper@hotmail.com>
2020-08-31 14:25:31 +0200

    pet Repository done in infrastructure

--- Infrastructure.SQL/Repositories/PetRepository.cs
+++ Infrastructure.SQL/Repositories/PetRepository.cs
@@ -1,7 +1,50 @@
-﻿namespace Infrastructure.SQL.Repositories
+﻿using System.Collections.Generic;
+using System.Linq;
+using Heroes.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.SQL.Repositories
 {
     public class PetRepository
     {
-        
+
+        private readonly DatabaseContext _context;
+
+        public PetRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public Pet CreatePet(Pet createdPet)
+        {
+            _context.Attach(createdPet).State = EntityState.Added;
+            _context.SaveChanges();
+            return createdPet;
+        }
+
+        public Pet DeletePet(int id)
+        {
+            var coverToRemove = _context.Remove(new Pet { Id = id }).Entity;
+            _context.SaveChanges();
+            return coverToRemove;
+        }
+
+        public List<Pet> GetAllPet()
+        {
+            return _context.Pets.ToList();
+        }
+
+        public Pet GetPetById(int id)
+        {
+            return _context.Pets.FirstOrDefault(c => c.Id == id);
+        }
+
+        public Pet UpdatePet(Pet updatedPet)
+        {
+            _context.Attach(updatedPet).State = EntityState.Modified;
+            _context.Update(updatedPet);
+            _context.SaveChanges();
+            return updatedPet;
+        }
     }
 }
